bafc680398466aae96821cbe80c56ccd12bb6885
Christoph Klassen <christoph.klassen@intevation.de>
2025-08-01 14:20:37 +0200

    Add base parameter
    Necessary because the demo is in a nested public path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueDevTools(), Components()],
   resolve: {
     alias: {
